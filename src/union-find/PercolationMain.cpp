b7bcb9abdf94be89c9983133ed76aba2bd288d6b
Denys Asauliak <d.asauliak@gmail.com>
2018-11-06 00:15:37 +0200

    rewrite percolation target as google test

--- src/union-find/PercolationMain.cpp
+++ src/union-find/PercolationMain.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <random>
 #include <numeric>
+#include <random>
+
+#include <gtest/gtest.h>
 
 #include "Percolation.hpp"
 
@@ -12,51 +14,73 @@ using namespace algorithms::uf;
 const std::size_t NUMBER_OF_SAMPLES = 100;
 
 /**
- * The width of experimental model.
- */
-const std::size_t WIDTH_OF_MODEL = 100;
-
-/**
  * Simulate on model with given width and randomly opened sites.
  *
+ * Used monte carlo method to open sites in percolation model.
+ *
  * The percolation threshold indicates amount of percents of opened sites
- * in percolation model which percolated with high probability.
+ * in percolation model. It shows probability that percolation model
+ * is percolated from top to bottom.
  *
  * @param width The width of model.
  *
  * @return The percolation threshold.
  */
-double simulate(std::size_t width)
+double simulate(Percolation& model)
 {
-    Percolation percolation(width);
-
     std::random_device device;
     std::mt19937 engine(device());
-    std::uniform_int_distribution<std::size_t> dist(0, percolation.totalCount() - 1);
+    std::uniform_int_distribution<std::size_t> dist(0, model.totalCount() - 1);
 
-    bool p = false;
-    while (!p) {
+    bool percolated = false;
+    while (!percolated) {
         std::size_t site = dist(engine);
-        if (!percolation.isOpened(site)) {
-            percolation.open(site);
+        if (!model.isOpened(site)) {
+            model.open(site);
+            percolated = model.isPercolated();
         }
-        p = percolation.isPercolated();
     }
 
-    std::size_t opened = percolation.countOfOpened();
-    double threshold = double(opened) / percolation.totalCount();
+    std::size_t opened = model.countOfOpened();
+    double threshold = double(opened) / model.totalCount();
     return threshold;
 }
 
-int main()
+/**
+ * Calculate percolation threshold by calculating mean value between
+ * some amount of samples.
+ *
+ * @param width The width of model.
+ *
+ * @return The percolation threshold.
+ */
+double calculate(Percolation& model)
 {
     std::vector<double> values(NUMBER_OF_SAMPLES);
     for (std::size_t i = 0; i < NUMBER_OF_SAMPLES; ++i) {
-        values[i] = simulate(WIDTH_OF_MODEL);
+        values[i] = simulate(model);
     }
-
     double sum = std::accumulate(values.cbegin(), values.cend(), 0.0);
-    double mean = sum / values.size();
+    return sum / values.size();
+}
 
-    std::cout << "The percolation threshold: " << mean << std::endl;
+TEST(Percolation, w100)
+{
+    Percolation model(100);
+    std::cout << "The percolation threshold: " << calculate(model) << std::endl;
+    ASSERT_TRUE(model.isPercolated());
+}
+
+TEST(Percolation, w500)
+{
+    Percolation model(500);
+    std::cout << "The percolation threshold: " << calculate(model) << std::endl;
+    ASSERT_TRUE(model.isPercolated());
+}
+
+TEST(Percolation, w1000)
+{
+    Percolation model(1000);
+    std::cout << "The percolation threshold: " << calculate(model) << std::endl;
+    ASSERT_TRUE(model.isPercolated());
 }
