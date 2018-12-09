978e682e73e878e711f9bc84c4023abc4863bfea
D.Asauliak <d.asauliak@gmail.com>
2018-12-09 23:27:15 +0200

    imp bottom up merge sort algo

---
+++ src/sort/BottomUpMerge.hpp
@@ -0,0 +1,70 @@
+#ifndef BOTTOMUPMERGE_HPP
+#define BOTTOMUPMERGE_HPP
+
+#include <vector>
+#include <algorithm>
+
+#include "Sort.hpp"
+
+namespace algorithms {
+
+class BottomUpMerge : public Sort {
+public:
+    template <typename RandomIt>
+    static void sort(RandomIt first, RandomIt last)
+    {
+        using T = typename std::iterator_traits<RandomIt>::value_type;
+
+        sort(first, last, std::less<T>{});
+    }
+
+    template <typename RandomIt, typename Less>
+    static void sort(RandomIt first, RandomIt last, Less less)
+    {
+        using T = typename std::iterator_traits<RandomIt>::value_type;
+
+        if (first >= last) {
+            return;
+        }
+
+        std::size_t N = std::distance(first, last);
+        std::vector<T> buffer(N);
+        for (std::size_t sz = 1; sz < N; sz += sz) {
+            for (std::size_t lo = 0; lo < N - sz; lo += 2 * sz) {
+                merge(first, buffer.begin(), less, lo, lo + sz - 1, std::min(lo + 2 * sz - 1, N - 1));
+            }
+        }
+    }
+
+private:
+    template <typename RandomIt, typename Less>
+    static void merge(RandomIt input, RandomIt output, Less less, std::size_t lo, std::size_t mid, std::size_t hi)
+    {
+        for (std::size_t k = lo; k <= hi; ++k) {
+            output[k] = input[k];
+        }
+
+        std::size_t i = lo;
+        std::size_t j = mid + 1;
+        for (std::size_t k = lo; k <= hi; ++k) {
+            if (i > mid) {
+                input[k] = output[j++];
+                continue;
+            }
+            if (j > hi) {
+                input[k] = output[i++];
+                continue;
+            }
+            if (less(output[j], output[i])) {
+                input[k] = output[j++];
+            }
+            else {
+                input[k] = output[i++];
+            }
+        }
+    }
+};
+
+} // namespace algorithms
+
+#endif // BOTTOMUPMERGE_HPP
---
+++ src/sort/BottomUpMergeMain.cpp
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+
+#include <Tools.hpp>
+
+#include "BottomUpMerge.hpp"
+
+using namespace algorithms;
+
+TEST(BottomUpMerge, Sort)
+{
+    Numbers numbers{2, 4, 3, 0, 5, 7, 6, 8};
+
+    auto time = measure([&] () {
+        BottomUpMerge::sort(numbers.begin(), numbers.end());
+    });
+    printMeasure("BottomUpMerge - 1K", time);
+
+    ASSERT_TRUE(BottomUpMerge::isAscending(numbers.begin(), numbers.end()));
+}
+
+TEST(BottomUpMerge, Sort1K)
+{
+    Numbers numbers = RandomGenerator::generate(1000);
+
+    auto time = measure([&] () {
+        BottomUpMerge::sort(numbers.begin(), numbers.end());
+    });
+    printMeasure("BottomUpMerge - 1K", time);
+
+    ASSERT_TRUE(BottomUpMerge::isAscending(numbers.begin(), numbers.end()));
+}
+
+TEST(BottomUpMerge, Sort10K)
+{
+    Numbers numbers = RandomGenerator::generate(10000);
+
+    auto time = measure([&] () {
+        BottomUpMerge::sort(numbers.begin(), numbers.end());
+    });
+    printMeasure("BottomUpMerge - 10K", time);
+
+    ASSERT_TRUE(BottomUpMerge::isAscending(numbers.begin(), numbers.end()));
+}
+
+TEST(BottomUpMerge, Sort100K)
+{
+    Numbers numbers = RandomGenerator::generate(100000);
+
+    auto time = measure([&] () {
+        BottomUpMerge::sort(numbers.begin(), numbers.end());
+    });
+    printMeasure("BottomUpMerge - 100K", time);
+
+    ASSERT_TRUE(BottomUpMerge::isAscending(numbers.begin(), numbers.end()));
+}
