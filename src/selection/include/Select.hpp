fc4f52d693f58a8751d0d03086958297cbecbb57
Denys Asauliak <d.asauliak@gmail.com>
2020-01-06 14:01:18 +0200

    changes comments

--- src/selection/include/Select.hpp
+++ src/selection/include/Select.hpp
@@ -11,13 +11,13 @@ namespace algorithms {
 
 class Select {
 public:
-    template <typename T, typename RandomIt>
+    template<typename T, typename RandomIt>
     static T get(RandomIt first, RandomIt last, std::size_t k)
     {
         return get<T>(first, last, std::less<T>{}, k);
     }
 
-    template <typename T, typename RandomIt, typename Less>
+    template<typename T, typename RandomIt, typename Less>
     static T get(RandomIt first, RandomIt last, Less less, std::size_t k)
     {
         if (first >= last) {
@@ -34,37 +34,45 @@ public:
         std::size_t lo = 0, hi = size - 1;
         while (hi > lo) {
             std::size_t j = partition(first, less, lo, hi);
-            if (j == k) {           // Target item has found
-                return first[k];    //
+            if (j == k) {
+                /** Target item has found */
+                return first[k];
             }
-            if (k > j) {            // Target item on the right part
-                lo = j + 1;         //
+            if (k > j) {
+                /** Target item on the right part */
+                lo = j + 1;
             }
-            if (k < j) {            // Target item on the left part
-                hi = j - 1;         //
+            if (k < j) {
+                /** Target item on the left part */
+                hi = j - 1;
             }
         }
         return first[k];
     }
 
 private:
-    template <typename RandomIt, typename Less>
+    template<typename RandomIt, typename Less>
     static std::size_t partition(RandomIt input, Less less, std::size_t lo, std::size_t hi)
     {
         std::size_t i = lo;
         std::size_t j = hi + 1;
         while (true) {
-            while (less(input[++i], input[lo])) {   //
-                if (i == hi) break;                 //     Find item from left to right
-            }                                       // (continue while input[i] < input[lo])
+            while (less(input[++i], input[lo])) { ///      Find item from left to right
+                if (i == hi)                      /// (continue while input[i] < input[lo])
+                    break;                        ///
+            }
 
-            while (less(input[lo], input[--j])) {   //
-                if (j == lo) break;                 //     Find item from right to left
-            }                                       // (continue while input[j] > input[lo])
+            while (less(input[lo], input[--j])) { ///      Find item from right to left
+                if (j == lo)                      /// (continue while input[j] > input[lo])
+                    break;                        ///
+            }
 
-            if (i >= j) break;                      // Quit when i and j crossed
+            /** Quit when i and j crossed */
+            if (i >= j)
+                break;
 
-            std::iter_swap(input + i, input + j);   // Exchange items to the right place
+            /** Exchange items to the right place */
+            std::iter_swap(input + i, input + j);
         }
         std::iter_swap(input + lo, input + j);
         return j;
