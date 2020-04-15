442481e50c1a23d4f070423f4eceded56a6925c3
Denys Asauliak <d.asauliak@gmail.com>
2020-04-15 22:48:35 +0300

    rework using gtets on project

--- cmake/external/GoogleTest.cmake
+++ cmake/external/GoogleTest.cmake
@@ -1,61 +1,25 @@
-include(FetchContent)
+list(APPEND CMAKE_PREFIX_PATH "~/opt/googletest")
 
-set(_NAME googletest)
-set(_TAG master)
-set(_URL https://github.com/google/googletest)
+find_package(GTest CONFIG REQUIRED)
 
-FetchContent_Declare(
-  ${_NAME}
-  GIT_REPOSITORY ${_URL}
-  GIT_TAG ${_TAG}
-)
-
-FetchContent_GetProperties(${_NAME})
-if(${_NAME}_POPULATED)
-    message(STATUS "${_NAME} has already populated")
-    return()
+if (TARGET GTest::gtest)
+    get_property(GTEST_LOCATION
+        TARGET
+            GTest::gtest
+        PROPERTY
+            LOCATION)
+    message(STATUS "Found GTest :${GTEST_LOCATION}")
+else()
+    message(FATAL_ERROR "GTest library NOT found")
 endif()
-FetchContent_Populate(${_NAME})
 
-set(_SOURCE_DIR ${${_NAME}_SOURCE_DIR})
-set(_BINARY_DIR ${${_NAME}_BINARY_DIR})
-
-#
-# Configure and compile
-#
-execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" ${_SOURCE_DIR}
-    WORKING_DIRECTORY "${_BINARY_DIR}" )
-execute_process(COMMAND "${CMAKE_COMMAND}" --build . --parallel
-    WORKING_DIRECTORY "${_BINARY_DIR}" )
-
-find_package(Threads REQUIRED)
-
-#
-# Define GTest library target
-#
-add_library(${_NAME}-gtest INTERFACE IMPORTED GLOBAL)
-target_link_libraries(${_NAME}-gtest
-    INTERFACE
-        ${_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}
-        ${_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}
-    )
-target_include_directories(${_NAME}-gtest
-    INTERFACE ${_SOURCE_DIR}/googletest/include)
-set_property(TARGET ${_NAME}-gtest
-             APPEND
-             PROPERTY INTERFACE_LINK_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
-
-#
-# Define GMock library target
-#
-add_library(${_NAME}-gmock INTERFACE IMPORTED GLOBAL)
-target_link_libraries(${_NAME}-gmock
-    INTERFACE
-        ${_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}
-        ${_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gmock_main${CMAKE_STATIC_LIBRARY_SUFFIX}
-    )
-target_include_directories(${_NAME}-gmock
-    INTERFACE ${_SOURCE_DIR}/googlemock/include)
-set_property(TARGET ${_NAME}-gmock
-             APPEND
-             PROPERTY INTERFACE_LINK_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
+if (TARGET GTest::gmock)
+    get_property(GMOCK_LOCATION
+        TARGET
+            GTest::gmock
+        PROPERTY
+            LOCATION)
+    message(STATUS "Found GMock :${GMOCK_LOCATION}")
+else()
+    message(FATAL_ERROR "GMock library NOT found")
+endif()
