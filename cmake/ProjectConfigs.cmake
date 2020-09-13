914982a10a2740390c9251a48c479531670d0836
Denys Asauliak <d.asauliak@gmail.com>
2020-09-13 18:42:13 +0300

    add cmake module in order to enable ccache

--- cmake/ProjectConfigs.cmake
+++ cmake/ProjectConfigs.cmake
@@ -2,6 +2,7 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
 
 include(BuildLocation)
 include(BuildType)
+include(EnableCcache)
 
 include(CxxTestCompiler)
 checkForCxx17Compiler(CXX17_COMPILER)
